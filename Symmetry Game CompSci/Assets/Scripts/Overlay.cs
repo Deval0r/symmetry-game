6badf40de91b65e852eb49ac96f175bbb45a841c
Deval0r <jpennock5701@student.pps.net>
2025-01-08 15:30:24 -0800

    more camera goof
    player moves more right when going right, does not move when going left

--- Symmetry Game CompSci/Assets/Scripts/Overlay.cs
+++ Symmetry Game CompSci/Assets/Scripts/Overlay.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class Overlay : MonoBehaviour
 {
     private Camera mainCamera;
+    private Vector3 initialScale;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // Get the main camera
         mainCamera = Camera.main;
+        // Store the initial scale of the object
+        initialScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -18,5 +21,9 @@ public class Overlay : MonoBehaviour
         Vector3 screenCenter = new Vector3(0.5f, 0.5f, mainCamera.nearClipPlane);
         Vector3 worldPosition = mainCamera.ViewportToWorldPoint(screenCenter);
         transform.position = worldPosition;
+
+        // Adjust the scale of the object to remain visually consistent
+        float scaleFactor = mainCamera.orthographicSize / 5f; // Adjust the denominator as needed
+        transform.localScale = initialScale * scaleFactor;
     }
 }
